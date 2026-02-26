86832d9c60c8718cf0daca8fe268b4b055b3f680
ilyas-g <ilyas15069i@gmail.com>
2026-02-26 15:40:38 +0100

    Ajout d'éléments pour les dataframes

--- api/velib_metropole_informations.py
+++ api/velib_metropole_informations.py
@@ -7,7 +7,7 @@ from retry_requests import retry
 
 INFO_URL = "https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_information.json"
 
-def fetch_velib():
+def fetch_velib_informations():
     # Setup the Velib API client with cache and retry on error
     cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
     retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
@@ -18,17 +18,6 @@ def fetch_velib():
         response = retry_session.get(INFO_URL, timeout=10)  # Timeout de 10 secondes
         response.raise_for_status()  # Vérifier les erreurs HTTP
         data = response.json()
-        print("✓ Data fetched successfully!")
-
-        stations_data = data['data']['stations']
-
-        df = pd.DataFrame(stations_data)
-        df['date'] = pd.Timestamp.today().date()
-        df = df.drop(columns=['rental_methods'])
-        # df = df.astype('object').replace({np.nan: None})
-        print(df.head())
-
-
         # print(data['data']['stations'])  # Time to live for the data
     except requests.exceptions.Timeout:
         print("❌ Timeout: La requête a pris trop de temps")
@@ -38,10 +27,22 @@ def fetch_velib():
         print(f"❌ Error: {e}")
     # Process the data as needed
     # For example, you can convert it to a DataFrame
+
+    print("✓ Data fetched successfully!")
+    stations_data = data['data']['stations']
+
+    df = pd.DataFrame(stations_data)
+    # df = df.drop(columns=['rental_methods'])
+    df = df.drop(columns=['station_opening_hours'])
+    # print(df['rental_methods'].head())
+    # print(df.head())  # Print the first few rows of the DataFrame
+    # print(df.columns.tolist())  # Print the column names
+    print(df['rental_methods'].iloc[0])  # Premier élément
+    print(type(df['rental_methods'].iloc[0]))  # Type de données
     # stations_data = data['data']['stations']
     # df_stations = pd.DataFrame(stations_data)
 
     # print(df_stations.head())  # Print the first few rows of the DataFrame
     # return df_stations
 
-fetch_velib()
+fetch_velib_informations()
--- api/velib_metropole_stations.py
+++ api/velib_metropole_stations.py
@@ -7,7 +7,7 @@ from retry_requests import retry
 
 STATUS_URL = "https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_status.json"
 
-def fetch_velib():
+def fetch_velib_stations():
     # Setup the Velib API client with cache and retry on error
     cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
     retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
@@ -18,17 +18,6 @@ def fetch_velib():
         response = retry_session.get(STATUS_URL, timeout=10)  # Timeout de 10 secondes
         response.raise_for_status()  # Vérifier les erreurs HTTP
         data = response.json()
-        
-
-        # stations_data = data['data']['stations']
-
-        # df = pd.DataFrame(stations_data)
-        # df['date'] = pd.Timestamp.today().date()
-        # df = df.drop(columns=['num_bikes_available_types'])
-
-        # # df = df.astype('object').replace({np.nan: None})
-        # print(df)
-
     except requests.exceptions.Timeout:
         print("❌ Timeout: La requête a pris trop de temps")
     except requests.exceptions.ConnectionError:
@@ -41,13 +30,28 @@ def fetch_velib():
     print("✓ Data fetched successfully!")
     stations_data = data['data']['stations']
     df = pd.DataFrame(stations_data)
-    df['last_reported'] = pd.to_datetime(df['last_reported'], unit='s').dt.date
-    df['date'] = pd.Timestamp.today().date()
+    df['last_reported'] = pd.to_datetime(df['last_reported'], unit='s').dt.strftime('%d-%m-%Y')
+    df['date'] = pd.Timestamp.today().strftime('%d-%m-%Y')
+    # Supprimer les colonnes redondantes (camelCase)
+    df = df.drop(columns=['numBikesAvailable', 'numDocksAvailable'])
     df = df.drop(columns=['num_bikes_available_types'])
+
+    # Process the num_bikes_available_types to create separate columns for each bike type
+    # This is a placeholder for the actual processing logic, which will depend on the structure of num_bikes_available_types
+    # C'EST A UTILSER POUR TRAITER LES TYPES DE VÉLOS DISPONIBLES, SI NÉCESSAIRE
+    # for _, row in df.iterrows():
+    #     for d in row['num_bikes_available_types']:
+    #         bike_type, cnt = next(iter(d.items()))
+    #         # INSERT into station_bike_counts …
+    
+    cols = df.columns.tolist()
+    cols.insert(1, cols.pop(cols.index('stationCode')))
+    cols.insert(2, cols.pop(cols.index('date')))
+    df = df[cols]
     # # df = df.astype('object').replace({np.nan: None})
 
     print(df.head())  # Print the first few rows of the DataFrame
-    # print(df.columns.tolist())  # Print the column names
+    print(df.columns.tolist())  # Print the column names
     # return df_stations
 
-fetch_velib()
+fetch_velib_stations()
